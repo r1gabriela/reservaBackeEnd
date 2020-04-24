3d44d3ac8164a9460d17c6e2b908cb52de990b98
gv92769 <gv92769@gmail.com>
2020-04-24 14:06:02 -0300

    TASK#92769-40: Funcionalidade Cadastrar/Editar Funcionário

--- src/main/java/com/lpi/reserva/Repository/FuncionarioRepository.java
+++ src/main/java/com/lpi/reserva/Repository/FuncionarioRepository.java
@@ -3,9 +3,9 @@ package com.lpi.reserva.Repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.lpi.reserva.entity.Dependente;
+import com.lpi.reserva.entity.Funcionario;
 
 @Repository
-public interface DependenteRepository extends CrudRepository<Dependente, Integer> {
+public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
 
 }
---
+++ src/main/java/com/lpi/reserva/dto/FuncionarioDto.java
@@ -0,0 +1,13 @@
+package com.lpi.reserva.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @AllArgsConstructor @Getter @Setter
+public class FuncionarioDto extends PessoaDto {
+
+	private Integer idTipoFuncionario;
+	
+}
