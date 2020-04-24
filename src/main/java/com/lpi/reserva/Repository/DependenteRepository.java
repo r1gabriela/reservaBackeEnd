9f2782850ae41a67b3f28a13e0185b5bc7fbb203
r1gabriela <andradegabriela20@gmail.com>
2020-04-24 13:11:44 -0300

    TASK#92769-38: Funcionalidade Excluir Dependente

--- src/main/java/com/lpi/reserva/Repository/DependenteRepository.java
+++ src/main/java/com/lpi/reserva/Repository/DependenteRepository.java
@@ -3,9 +3,10 @@ package com.lpi.reserva.Repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.lpi.reserva.entity.Dependente;
 import com.lpi.reserva.entity.Pessoa;
 
 @Repository
-public interface DependenteRepository extends CrudRepository<Pessoa, Integer> {
+public interface DependenteRepository extends CrudRepository<Dependente, Integer> {
 
 }
