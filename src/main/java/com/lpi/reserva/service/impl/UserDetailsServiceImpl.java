0b1d2e437d656f650c3c903f1f3277572cdea104
gv92769 <gv92769@gmail.com>
2020-05-09 13:16:39 -0300

    TASK#92769-70: Implementar Login Backend

--- src/main/java/com/lpi/reserva/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/lpi/reserva/service/impl/UserDetailsServiceImpl.java
@@ -5,12 +5,15 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lpi.reserva.Repository.UsuarioRepository;
+import com.lpi.reserva.entity.Privilegio;
 import com.lpi.reserva.entity.Usuario;
 
 @Service
@@ -20,16 +23,20 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	private UsuarioRepository usuarioRepository;
 	
 	@Override
-	public UserDetails loadUserByUsername(String login)
-			throws UsernameNotFoundException {
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
 		Usuario usuario = usuarioRepository.pesquisarUsuarioPorLogin(login);
+		
 		if(usuario == null) {
 			throw new UsernameNotFoundException("Login não encontrado");
 		}
 		
 		Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
 		
-		return new org.springframework.security.core.userdetails.User("", "", grantedAuthorities);
+		for (Privilegio privilegio: usuario.getRole().getPrivilegios())
+			grantedAuthorities.add(new SimpleGrantedAuthority(privilegio.getNome()));
+		
+		return new org.springframework.security.core.userdetails.User(usuario.getLogin(), usuario.getSenha(), grantedAuthorities);
 	}
 
 }
