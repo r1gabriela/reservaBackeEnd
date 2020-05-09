cbe9770b015228b96790b5f72e3e598662cf5d2e
r1gabriela <andradegabriela20@gmail.com>
2020-05-09 16:17:20 -0300

    BUG#92769-89: Correção de Bug na inicialização de Beans

--- src/main/java/com/lpi/reserva/SecurityConfig.java
+++ src/main/java/com/lpi/reserva/SecurityConfig.java
@@ -1,14 +1,15 @@
 package com.lpi.reserva;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -22,7 +23,8 @@ import com.lpi.reserva.service.impl.UserDetailsServiceImpl;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
-	final  UserDetailsService userDetailsService = new UserDetailsServiceImpl();
+	@Autowired
+	UserDetailsServiceImpl userDetailsService;
 	
 	//@Override
 	//public void configure(WebSecurity web) throws Exception{
@@ -81,5 +83,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 	return  source;
 	}
+		
+	@Bean
+    public AuthenticationManager customAuthenticationManager() throws Exception {
+        return authenticationManager();
+    }
 	
 }
