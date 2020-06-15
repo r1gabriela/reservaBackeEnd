af98120cee22da8310a7a39fed579e9a34a28605
r1gabriela <andradegabriela20@gmail.com>
2020-06-15 00:27:04 -0300

    BUG#92769-170: Concertar bug da validação de CPF tela manter dependente

--- src/main/java/com/lpi/reserva/dto/DependenteDto.java
+++ src/main/java/com/lpi/reserva/dto/DependenteDto.java
@@ -1,7 +1,5 @@
 package com.lpi.reserva.dto;
 
-import javax.validation.constraints.NotBlank;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +8,6 @@ import lombok.Setter;
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class DependenteDto extends PessoaDto {
 	
-	@NotBlank
 	private ClienteDto cliente;
 	
 	private Boolean ativo;
