f5c8561010f3c69305cd416accd659cc036318b6
gv92769 <gv92769@gmail.com>
2020-06-16 01:02:38 -0300

    BUG#92769-187: Correção de Bug em Funcionario

--- src/main/java/com/lpi/reserva/dto/FuncionarioDto.java
+++ src/main/java/com/lpi/reserva/dto/FuncionarioDto.java
@@ -1,7 +1,5 @@
 package com.lpi.reserva.dto;
 
-import javax.validation.constraints.NotBlank;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +8,6 @@ import lombok.Setter;
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class FuncionarioDto extends PessoaDto {
 	
-	@NotBlank
 	private TipoFuncionarioDto tipoFuncionario;
 	
 }
